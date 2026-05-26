4e2d25d8cba5d899ccabad3885070e4b875f04a4
Dinh Xuan Khuong <dxkhuong23@clc.fitus.edu.vn>
2026-05-26 16:09:12 +0700

    add ui

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ import streamlit as st
 import numpy as np
 import pandas as pd 
 import altair as alt
+from datetime import time, datetime
+
+
+RANDOM_SEED = 42
+
+np.random.seed(RANDOM_SEED)
 
 
 st.header("Hello, welcome to my streamlit website")
 st.write("This is the project showing my journey of 30 days of Streamlit")
 st.subheader("This is a subheader", divider = True)
 st.caption("This is an caption")
+
+st.subheader("Working with data", divider = True)
 
 df = pd.DataFrame({"Name": ["Dinh Xuan Khuong"], "Age": [21], "Email": ["dinhxuankhuong2005@gmail.com"]})
 
@@ -21,3 +29,64 @@ df2 = pd.DataFrame(
 c = alt.Chart(df2).mark_circle().encode(
      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
+
+st.subheader("Let's learn about slider", divider = True)
+st.write("You can write code like this")
+
+
+code_body = """
+import streamlit as st
+
+age = st.slider("how old are you?", 0, 130, 25)
+
+# 0, 130, 25 represent the minimum, maximum and default values, 
+
+st.write("Im ", age, " year olds")
+
+
+""".strip()
+
+st.code(code_body , "python")
+
+age = st.slider("how old are you?", 0, 130, 25)
+st.write("Im ", age, " year olds")
+
+st.subheader('Range slider')
+
+values = st.slider("Select a range of values", 0.0, 100.0, (0.25, 0.75))
+
+st.write('Values:', values)
+
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30), # The default value for the datetime was set using the value option to be January 1, 2020 at 9:30
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
+
+st.header("st.line_chart")
+
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+st.header('st.selectbox')
+
+option = st.selectbox("Choose your favorite color: ", ("Blue", "Green", "Yellow"))
+
+st.write(f"Your favorite color is: ", option)
+
+st.header('st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red']) # Default
+
+st.write('You selected:', options)
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Khuong is doing streamlit
+
+You can check my deployed website here: [website](https://khuong-is-doing-app-fmjz3cwmdesthrm7tvrhur.streamlit.app/)
+
